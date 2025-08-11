0e94feeba986c3c568e883724d6e13f6dd60558a
ZzzhangLK <656650937@qq.com>
2025-08-11 15:44:51 +0800

    feat(i18n): 添加国际化支持并优化主题样式
    -引入 Ant Design 国际化支持，设置中文语言环境
    - 添加自定义字体样式，提升用户体验
    - 调整页面底部信息的字体权重，增强可读性

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import App from './App';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ConfigProvider
+        locale={zhCN}
+        theme={{
+          token: {
+            fontFamily: `'Noto Serif SC', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
